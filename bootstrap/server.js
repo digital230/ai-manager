690c674275d2dc8baa1eaed7eff55127254644ba
digital230 <digit230@gmail.com>
2018-09-08 22:10:54 +0500

    port updated

--- bootstrap/server.js
+++ bootstrap/server.js
@@ -3,7 +3,7 @@ import restify from 'restify';
 
 export default () => {
   const server = restify.createServer();
-  const port = process.env.port || process.env.PORT || 8080;
+  const port = process.env.port || process.env.PORT || 3978;
   server.listen(port, () => {
      console.log('%s listening to %s', server.name, server.url);
   });
