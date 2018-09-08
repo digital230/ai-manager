7456e4fe618aa274c6d25fdbed59318e31eb11a1
digital230 <digit230@gmail.com>
2018-09-08 23:25:23 +0500

    testing user

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export default (connector) => {
   const inMemoryStorage = new builder.MemoryBotStorage();
 
   const bot = new builder.UniversalBot(connector, function (session) {
+    console.log(session.message.address)
     session.send("You said: %s", session.message.text);
   }).set('storage', inMemoryStorage);
 
